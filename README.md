ee48a45e092dfde0de7c6e1395e479bcf0c63fc9
Mohd_Labib <karibschool.web@gmail.com>
2023-10-04 00:53:52 +0700

    add tugas 2

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ demo: https://tugas-automata.surge.sh
 
   - Tugas 1: https://tugas-automata.surge.sh/tugas1.html
   - Tugas 2: https://tugas-automata.surge.sh/tugas2.html
+  - Tugas 2 - Alternatif: https://tugas-automata.surge.sh/tugas2-alternatif.html
+
+
+  * Tugas 2 hanya sedikit pengecekan tambahan di line 70. 
--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,15 @@ const stop = () => {
         confetti.stop()
     }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
 };
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+    }
+});
